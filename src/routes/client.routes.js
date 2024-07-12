b1dffa8c6212247ad9688f5c59f15d5315e1b091
luisillo77D <luisillo77mario@gmail.com>
2024-07-11 22:24:17 -0700

    chore: Refactor client controller and routes

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import { registerClient, getClients, getClientById, updateClientById, getClientByName } from "../controllers/client.controller.js";
+import { registerClient, getClients, getClientById, updateClientById, } from "../controllers/client.controller.js";
 
 const router = Router();
 
 router.post("/register", registerClient);
 router.get("/", getClients);
 router.get("/:clientId", getClientById);
-router.get("/name/:name", getClientByName);
 router.put("/:clientId", updateClientById);
 
 export default router;
