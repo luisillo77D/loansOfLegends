f94ad80e66c10a792b532efa32d21b5ecfbf23fd
luisillo77D <luisillo77mario@gmail.com>
2024-07-15 21:55:04 -0700

    feat: Add loan retrieval by ID, payment update, and weekly payment retrieval

--- src/controllers/loan.controller.js
+++ src/controllers/loan.controller.js
@@ -63,3 +63,40 @@ export const getLoans = async (req, res) => {
         console.error(error);
     }
 }
+
+//metodo para obtener un prestamo por id
+export const getLoanById = async (req, res) => {
+    try {
+        const { loanId } = req.params;
+        const loan = await Loan.findById(loanId);
+        if (!loan) return res.status(404).json({ message: "Loan not found" });
+        res.status(200).json(loan);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+//metodo para cambiar el estado de un prestamo a pagado
+export const payLoan = async (req, res) => {
+    try {
+        const { loanId } = req.params;
+        const loan = await Loan.findByIdAndUpdate(loanId, { paid: true }, { new: true });
+        res.status(200).json(loan);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+//metodo para obtener los pagos semanales de un prestamo
+export const getWeeklyPayments = async (req, res) => {
+    try {
+        const { loanId } = req.params;
+        const weeklyPayments = await WeeklyPayment.find({ loan: loanId, paid: false });
+        res.status(200).json(weeklyPayments);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
--- src/routes/loan.routes.js
+++ src/routes/loan.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerLoan, getActiveLoans, getLoans} from "../controllers/loan.controller.js";
+import { registerLoan, getActiveLoans, getLoans,getLoanById,payLoan,getWeeklyPayments} from "../controllers/loan.controller.js";
 import loanVerifications from "../middleware/registerLoan.middlewere.js";
 
 const router = Router();
@@ -7,5 +7,8 @@ const router = Router();
 router.post("/register", loanVerifications, registerLoan);
 router.get("/", getActiveLoans);
 router.get("/all", getLoans);
+router.get("/all/:loanId", getLoanById);
+router.put("/pay/:loanId", payLoan);
+router.get("/payments/:loanId", getWeeklyPayments);
 
 export default router;
